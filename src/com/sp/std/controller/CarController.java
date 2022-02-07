bf5b1e78f5748dc273157abad25f915d4ceb5180
wanglei60 <wanglei60@lenovo.com>
2022-02-07 11:49:14 +0800

    注解驱动 配置和实验2

--- src/com/sp/std/controller/CarController.java
+++ src/com/sp/std/controller/CarController.java
@@ -10,6 +10,9 @@ public class CarController {
     @Autowired
     @Qualifier("carService")
     //当有多个 实例对象被创建的时候 可以使用 @Qualifier 来指定不同的名称
-    // 避免多个时 不知道使用哪个，容器启动时会报错
+    //根据指定名称来匹配，不使用默认的 根据类的变量名来装配
+    //找到就装配 ，找不到就会报错
+    //如果找不到 不想报错 ，可以添加 required  属性 默认true
+    //指定@Autowired(required = false) 为false 会出现空指针异常
     private CarService carService;
 }
